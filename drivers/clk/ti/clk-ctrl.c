hansemro/u-boot

commit 215bd541b8bb71fa97b4dc158f7c533be23043ef

clk: ti: add support for clkctrl clocks
Until now the clkctrl clocks have been enabled/disabled through platform
routines. Thanks to this patch they can be enabled and configured directly
by the probed devices that need to use them.

For DT binding details see Linux doc:
- Documentation/devicetree/bindings/clock/ti-clkctrl.txt

Signed-off-by: Dario Binacchi <dariobin@libero.it>

drivers/clk/ti/clk-ctrl.c
@@ -1,0 +1,154 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * OMAP clock controller support
+ *
+ * Copyright (C) 2020 Dario Binacchi <dariobin@libero.it>
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <dm/device_compat.h>
+#include <clk-uclass.h>
+#include <asm/arch-am33xx/clock.h>
+
+struct clk_ti_ctrl_offs {
+	fdt_addr_t start;
+	fdt_size_t end;
+};
+
+struct clk_ti_ctrl_priv {
+	int offs_num;
+	struct clk_ti_ctrl_offs *offs;
+};
+
+static int clk_ti_ctrl_check_offs(struct clk *clk, fdt_addr_t offs)
+{
+	struct clk_ti_ctrl_priv *priv = dev_get_priv(clk->dev);
+	int i;
+
+	for (i = 0; i < priv->offs_num; i++) {
+		if (offs >= priv->offs[i].start && offs <= priv->offs[i].end)
+			return 0;
+	}
+
+	return -EFAULT;
+}
+
+static int clk_ti_ctrl_disable(struct clk *clk)
+{
+	struct clk_ti_ctrl_priv *priv = dev_get_priv(clk->dev);
+	u32 *clk_modules[2] = { };
+	fdt_addr_t offs;
+	int err;
+
+	offs = priv->offs[0].start + clk->id;
+	err = clk_ti_ctrl_check_offs(clk, offs);
+	if (err) {
+		dev_err(clk->dev, "invalid offset: 0x%lx\n", offs);
+		return err;
+	}
+
+	clk_modules[0] = (u32 *)(offs);
+	dev_dbg(clk->dev, "module address=%p\n", clk_modules[0]);
+	do_disable_clocks(NULL, clk_modules, 1);
+	return 0;
+}
+
+static int clk_ti_ctrl_enable(struct clk *clk)
+{
+	struct clk_ti_ctrl_priv *priv = dev_get_priv(clk->dev);
+	u32 *clk_modules[2] = { };
+	fdt_addr_t offs;
+	int err;
+
+	offs = priv->offs[0].start + clk->id;
+	err = clk_ti_ctrl_check_offs(clk, offs);
+	if (err) {
+		dev_err(clk->dev, "invalid offset: 0x%lx\n", offs);
+		return err;
+	}
+
+	clk_modules[0] = (u32 *)(offs);
+	dev_dbg(clk->dev, "module address=%p\n", clk_modules[0]);
+	do_enable_clocks(NULL, clk_modules, 1);
+	return 0;
+}
+
+static ulong clk_ti_ctrl_get_rate(struct clk *clk)
+{
+	return 0;
+}
+
+static int clk_ti_ctrl_of_xlate(struct clk *clk,
+				struct ofnode_phandle_args *args)
+{
+	if (args->args_count != 2) {
+		dev_err(clk->dev, "invaild args_count: %d\n", args->args_count);
+		return -EINVAL;
+	}
+
+	if (args->args_count)
+		clk->id = args->args[0];
+	else
+		clk->id = 0;
+
+	dev_dbg(clk->dev, "name=%s, id=%ld\n", clk->dev->name, clk->id);
+	return 0;
+}
+
+static int clk_ti_ctrl_of_to_plat(struct udevice *dev)
+{
+	struct clk_ti_ctrl_priv *priv = dev_get_priv(dev);
+	fdt_size_t fdt_size;
+	int i, size;
+
+	size = dev_read_size(dev, "reg");
+	if (size < 0) {
+		dev_err(dev, "failed to get 'reg' size\n");
+		return size;
+	}
+
+	priv->offs_num = size / 2 / sizeof(u32);
+	dev_dbg(dev, "size=%d, regs_num=%d\n", size, priv->offs_num);
+
+	priv->offs = kmalloc_array(priv->offs_num, sizeof(*priv->offs),
+				   GFP_KERNEL);
+	if (!priv->offs)
+		return -ENOMEM;
+
+	for (i = 0; i < priv->offs_num; i++) {
+		priv->offs[i].start =
+			dev_read_addr_size_index(dev, i, &fdt_size);
+		if (priv->offs[i].start == FDT_ADDR_T_NONE) {
+			dev_err(dev, "failed to get offset %d\n", i);
+			return -EINVAL;
+		}
+
+		priv->offs[i].end = priv->offs[i].start + fdt_size;
+		dev_dbg(dev, "start=0x%08lx, end=0x%08lx\n",
+			priv->offs[i].start, priv->offs[i].end);
+	}
+
+	return 0;
+}
+
+static struct clk_ops clk_ti_ctrl_ops = {
+	.of_xlate = clk_ti_ctrl_of_xlate,
+	.enable = clk_ti_ctrl_enable,
+	.disable = clk_ti_ctrl_disable,
+	.get_rate = clk_ti_ctrl_get_rate,
+};
+
+static const struct udevice_id clk_ti_ctrl_ids[] = {
+	{.compatible = "ti,clkctrl"},
+	{},
+};
+
+U_BOOT_DRIVER(clk_ti_ctrl) = {
+	.name = "ti_ctrl_clk",
+	.id = UCLASS_CLK,
+	.of_match = clk_ti_ctrl_ids,
+	.ofdata_to_platdata = clk_ti_ctrl_of_to_plat,
+	.ops = &clk_ti_ctrl_ops,
+	.priv_auto = sizeof(struct clk_ti_ctrl_priv),
+};

drivers/clk/ti/Kconfig
@@ -10,6 +10,12 @@ config CLK_TI_AM3_DPLL
 	  This enables the DPLL clock drivers support on AM33XX SoCs. The DPLL
 	  provides all interface clocks and functional clocks to the processor.
 
+config CLK_TI_CTRL
+	bool "TI OMAP4 clock controller"
+	depends on CLK && OF_CONTROL
+	help
+	  This enables the clock controller driver support on TI's SoCs.
+
 config CLK_TI_DIVIDER
 	bool "TI divider clock driver"
 	depends on CLK && OF_CONTROL && CLK_CCF

drivers/clk/ti/Makefile
@@ -6,6 +6,7 @@
 obj-$(CONFIG_ARCH_OMAP2PLUS) += clk.o
 
 obj-$(CONFIG_CLK_TI_AM3_DPLL) += clk-am3-dpll.o clk-am3-dpll-x2.o
+obj-$(CONFIG_CLK_TI_CTRL) += clk-ctrl.o
 obj-$(CONFIG_CLK_TI_DIVIDER) += clk-divider.o
 obj-$(CONFIG_CLK_TI_GATE) += clk-gate.o
 obj-$(CONFIG_CLK_TI_MUX) += clk-mux.o

commit 093b347ded06c2a0a5d1422d76a8b2e1f0741232

clk: ti: add clk-ctrl support for OMAP44XX/OMAP54XX
Commit 92cc4e1c21 introduces sysc bus driver but fails on platforms
without UCLASS_CLK clock control driver support. While commit 215bd541b8
introduces TI generic clock control driver, it includes am33xx clock
library which differs slightly from one used by omap44xx and omap54xx
platforms (in arch/mach-omap2/clocks-common.c). A symptom of these two
commits on omap44xx (and possibly omap54xx) includes ti-sysc failing to
get clocks when probing MMC.

```
U-Boot 2022.04-rc4-00078-gb7f4413abb (Mar 24 2022 - 22:57:24 -0700)

CPU  : OMAP4460-GP ES1.1
Model: TI OMAP4 PandaBoard
Board: OMAP4 Panda
I2C:   ready
DRAM:  1 GiB
Core:  102 devices, 6 uclasses, devicetree: separate
MMC:   ti_sysc target-module@d5000: failed to get fck clock
ti_sysc target-module@d5000: failed to release all clocks
ti_sysc target-module@d5000: failed to get fck clock
ti_sysc target-module@d5000: failed to release all clocks
mmc@0 - probe failed: -38
OMAP SD/MMC: 0ti_sysc target-module@d5000: failed to get fck clock
ti_sysc target-module@d5000: failed to release all clocks
```

Same MMC error occurs on 1st generation Kindle Fire (kc1 which is
currently unsupported) on versions with those two commits but also
prevents EMMC from working (whereas SD still remains functional on
panda).

While disabling TI_SYSC works as a workaround, having UCLASS_CLK driver
will be necessary for getting something like GPTimer PWM driver support
in the future.

Fixes: 215bd541b8 ("clk: ti: add support for clkctrl clocks")
Fixes: 92cc4e1c21 ("bus: ti: add minimal sysc interconnect target driver")
Signed-off-by: Hansem Ro <hansemro@outlook.com>

drivers/clk/ti/clk-ctrl.c
@@ -9,7 +9,11 @@
 #include <dm.h>
 #include <dm/device_compat.h>
 #include <clk-uclass.h>
+#if CONFIG_IS_ENABLED(AM33XX)
 #include <asm/arch-am33xx/clock.h>
+#else
+#include <asm/omap_common.h>
+#endif
 
 struct clk_ti_ctrl_offs {
 	fdt_addr_t start;
@@ -50,7 +54,11 @@ static int clk_ti_ctrl_disable(struct clk *clk)
 
 	clk_modules[0] = (u32 *)(offs);
 	dev_dbg(clk->dev, "disable module @ %p\n", clk_modules[0]);
+#if CONFIG_IS_ENABLED(AM33XX)
 	do_disable_clocks(NULL, clk_modules, 1);
+#else
+	do_disable_clocks(NULL, (u32 *)clk_modules, 1);
+#endif
 	return 0;
 }
 
@@ -70,7 +78,11 @@ static int clk_ti_ctrl_enable(struct clk *clk)
 
 	clk_modules[0] = (u32 *)(offs);
 	dev_dbg(clk->dev, "enable module @ %p\n", clk_modules[0]);
+#if CONFIG_IS_ENABLED(AM33XX)
 	do_enable_clocks(NULL, clk_modules, 1);
+#else
+	do_enable_clocks(NULL, NULL, (u32 *)clk_modules, 1);
+#endif
 	return 0;
 }
 